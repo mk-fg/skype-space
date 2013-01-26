813ad9b99d6fc11cd3e317acf945346fbc82e583
Mike Kazantsev <mk.fraggod@gmail.com>
2013-01-26 16:04:44 +0600

    Gave up on namespace-jail idea - too much trouble and apparmor seem to be better anyway

--- build_skype_env.bash
+++ build_skype_env.bash
@@ -3,29 +3,36 @@ set -e
 
 dir=skype_env
 
-echo "Building skype-env in: $dir"
+## CLI
+usage() {
+	echo >&2 "Usage: $0 [ --debug ]"
+	exit 0
+}
+[[ "$#" -gt 1 || ( -n "$1" && "$1" != '--debug' ) ]] && usage
+[[ -n "$1" ]] && debug=true || debug=
 
+echo "Building skype-env in: $dir"
 echo " - cleanup"
 rm -rf "$dir"
-
 echo " - copying skype"
 cp -R /opt/skype "$dir"
 
 echo " - copying dependency libs"
+lib="$dir"
 cp_deps() {
 	ldd "$1" |
-	awk '$3 {print $3}' |
+	awk '$3 {print $3; next} match($1,/^\//) {print $1}' |
 	while read dep
 	do
-		[[ -e "$dir"/lib/"$(basename "$dep")" ]] && continue
+		[[ -e "$lib"/"$(basename "$dep")" ]] && continue
 		cp_deps "$dep"
-		cp "$dep" "$dir"/lib/
+		cp "$dep" "$lib"/
 	done
 }
-mkdir "$dir"/lib
 cp_deps "$dir"/skype
 
-echo " - finished,"\
-	"bin: $(du -hs "$dir"/skype | cut -f1)"\
-	"libs: $(du -hs "$dir"/lib | cut -f1)"\
-	"total: $(du -hs "$dir" | cut -f1)"
+# Special ldd to use x86 ld.so
+sed 's|^RTLDLIST=.*|RTLDLIST=./ld-linux.so.2|' /usr/bin/ldd >"$dir"/ldd
+chmod +x "$dir"/ldd
+
+echo " - finished, du: $(du -hs "$dir" | cut -f1)"
