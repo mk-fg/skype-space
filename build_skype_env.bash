f91abc675176214e92ac186a886d2a4a5d4315e1
Mike Kazantsev <mk.fraggod@gmail.com>
2013-01-26 13:15:20 +0600

    +skype_exec (initial cffi-based ns-api test)

--- build_skype_env.bash
+++ build_skype_env.bash
@@ -12,12 +12,18 @@ echo " - copying skype"
 cp -R /opt/skype "$dir"
 
 echo " - copying dependency libs"
+cp_deps() {
+	ldd "$1" |
+	awk '$3 {print $3}' |
+	while read dep
+	do
+		[[ -e "$dir"/lib/"$(basename "$dep")" ]] && continue
+		cp_deps "$dep"
+		cp "$dep" "$dir"/lib/
+	done
+}
 mkdir "$dir"/lib
-ldd "$dir"/skype |
-awk '$3 {print $3}' |
-while read dep
-do cp "$dep" "$dir"/lib/
-done
+cp_deps "$dir"/skype
 
 echo " - finished,"\
 	"bin: $(du -hs "$dir"/skype | cut -f1)"\
---
+++ skype_exec.py
@@ -0,0 +1,102 @@
+#!/usr/bin/ppy
+#-*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function
+
+
+import itertools as it, operator as op, functools as ft
+from time import time, sleep
+import os, sys, logging, errno
+
+
+from cffi import FFI
+import capng
+
+# Try to work around insane "write_table" operations (which assume that
+#  they can just write lextab.py and yacctab.py in current dir), used by default.
+Lexer = LRGeneratedTable = None
+try: from ply.lex import Lexer
+except ImportError: pass
+try: from pycparser.ply.lex import Lexer
+except ImportError: pass
+try: from ply.yacc import LRGeneratedTable
+except ImportError: pass
+try: from pycparser.ply.yacc import LRGeneratedTable
+except ImportError: pass
+if Lexer: Lexer.writetab = lambda s,*a,**k: None
+if LRGeneratedTable: LRGeneratedTable.write_table = lambda s,*a,**k: None
+
+
+class NSControl(object):
+
+	def __init__(self, callback):
+		self.ffi = FFI()
+		self.ffi.cdef('int ns_clone(int (*fn)(void *));')
+
+		self.libc = self.ffi.verify('''
+			#include <sched.h>
+			#include <signal.h>
+
+			static char stack[{stack_size}];
+
+			static int ns_clone(int (*fn)(void *)) {{
+				int child_pid = clone( fn,
+					stack + {stack_size},
+					CLONE_NEWUTS | SIGCHLD, NULL );
+				return child_pid;
+			}}
+		'''.format(stack_size=1 * 2**20)) # 1 MiB
+
+		self.callback = callback
+
+	def child_func(self, null_p):
+		self.callback()
+		return 0
+
+	def clone(self):
+		child_pid = self.libc.ns_clone(
+			self.ffi.callback('int(void *)', self.child_func) )
+		if child_pid < 0:
+			err = self.ffi.errno
+			err, err_msg = (errno.errorcode[err], os.strerror(err))\
+				if err else ('none', 'errno was set to 0')
+			raise OSError('Failed to fork child process: [{}] {}'.format(err, err_msg))
+		return child_pid
+
+
+def ns_main():
+	print('waka waka')
+
+
+def main(argz=None):
+	import argparse
+	parser = argparse.ArgumentParser(description='collectd configuration file updater.')
+	parser.add_argument('--debug', action='store_true', help='Verbose operation mode.')
+	optz = parser.parse_args(argz or sys.argv[1:])
+
+	global log
+	logging.basicConfig(level=logging.DEBUG if optz.debug else logging.INFO)
+	log = logging.getLogger()
+
+	nsc = NSControl(ns_main)
+
+	# iddqd, idkfa
+	try:
+		assert not capng.capng_get_caps_process()
+		assert not capng.capng_update(
+			capng.CAPNG_ADD,
+			capng.CAPNG_EFFECTIVE,
+			capng.CAP_SYS_ADMIN )
+		assert not capng.capng_apply(capng.CAPNG_SELECT_CAPS)
+	except (OSError, AssertionError) as err:
+		log.fatal('Failed to enable necessary capabilities: {}'.format(err))
+		sys.exit(1)
+	else:
+		log.debug('Capabilities enabled: {}'.format(
+			capng.capng_print_caps_text(
+				capng.CAPNG_PRINT_BUFFER, capng.CAPNG_EFFECTIVE ) ))
+
+	# Start the namespace
+	nsc.clone()
+
+
+if __name__ == '__main__': main()
