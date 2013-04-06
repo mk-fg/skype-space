229adfa647e5ab46d9c4bc8c3a2729f56ed0c24c
Mike Kazantsev <mk.fraggod@gmail.com>
2013-04-06 14:26:00 +0600

    skyped: handle UnicodeDecodeErrors with proper "replace", not "backslashreplace" (which is only valid for encoding)

--- skyped/skyped.py
+++ skyped/skyped.py
@@ -44,7 +44,7 @@ def force_bytes(bytes_or_unicode, encoding='utf-8', errors='backslashreplace'):
 	if isinstance(bytes_or_unicode, bytes): return bytes_or_unicode
 	return bytes_or_unicode.encode(encoding, errors)
 
-def force_unicode(bytes_or_unicode, encoding='utf-8', errors='backslashreplace'):
+def force_unicode(bytes_or_unicode, encoding='utf-8', errors='replace'):
 	if isinstance(bytes_or_unicode, unicode): return bytes_or_unicode
 	return bytes_or_unicode.decode(encoding, errors)
 
